7b98e64d700fb6ebf117fa24cf14cd12df21af7d
Ken Hwong <ken_hwong@msn.com>
2017-11-10 01:47:56 +0800

    Recode xdata app.

--- XHelper/MainWindow.xaml.cs
+++ XHelper/MainWindow.xaml.cs
@@ -23,6 +23,39 @@ namespace XHelper
         public MainWindow()
         {
             InitializeComponent();
+            InitializeCommand();
         }
+
+        public RoutedCommand cmdLoadData = new RoutedCommand("CommandLoadData", typeof(MainWindow));
+        private void InitializeCommand()
+        {
+            var cbQuit = new CommandBinding(ApplicationCommands.Close,
+                (sender, e) => { Application.Current.Shutdown(); e.Handled = true; },
+                (sender, e) => { e.CanExecute = true; e.Handled = true; });
+            CommandBinding cbLoadData = new CommandBinding(cmdLoadData,
+                (sender, e) =>
+                {
+                    Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+                    //ofd.InitialDirectory = System.IO.Path.GetDirectoryName(XGlobal.DataFile);
+                    ofd.DefaultExt = "*.xdata";
+                    ofd.Filter = "Data Files(*.data)|*.data|Data Backup Files(*.bak)|*.bak|All(*.*)|*.*";
+                    if (ofd.ShowDialog() == true)
+                        if (MessageBox.Show($"Load Data File {ofd.FileName}", "Load Data...",
+                            MessageBoxButton.OKCancel,
+                            MessageBoxImage.Question,
+                            MessageBoxResult.OK) == MessageBoxResult.OK)
+                        {
+                            //ZoneHelper.FnContentDeserialize(ofd.FileName);
+                            //listInformation.SelectedIndex = listInformation.Items.Add($"{DateTime.Now:hh:MM:ss} - 加载新的数据文件{ofd.FileName}.");
+                        }
+                    e.Handled = true;
+                },
+                (sender, e) => { e.CanExecute = true; e.Handled = true; });
+
+            this.CommandBindings.AddRange(new CommandBinding[] { cbQuit });
+
+        }
+
+        private void listInformation_SelectionChanged(object sender, SelectionChangedEventArgs e) { var lb = sender as ListBox; lb.ScrollIntoView(lb.Items[lb.Items.Count - 1]); }
     }
 }
